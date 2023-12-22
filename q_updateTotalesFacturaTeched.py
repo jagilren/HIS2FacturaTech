cd7e4b2784ed0f6b4114c2facf9b851bdb7010ac
jagilren <jagilren@gmail.com>
2023-12-21 19:45:14 -0500

    Add file q_updateTotalesFacturaTeched.py

---
+++ q_updateTotalesFacturaTeched.py
@@ -0,0 +1,25 @@
+import pyodbc
+
+import txtlogs
+
+
+def updateTotalesFacturaTeched(facturaNumero):
+    try:
+        conn = pyodbc.connect('DSN=access2003DSN;')
+        # Create a cursor
+        cursor1 = conn.cursor()
+        boolfacturaTeched=-1
+
+        # Execute a SQL query
+        query = f'''UPDATE  [TOTALES]  set [TOTALES].[FacturaTeched]={boolfacturaTeched} 
+                WHERE  [TOTALES].[FacturaTeched]='{facturaNumero} AND [TOTALES].[isFE]=-1'''
+        cursor1.execute(query)
+        cursor1.commit()
+    except Exception as e:
+        txtlogs.writeLog(facturaNumero, f'q_updateTotalesFacturaTech-{str(e)}')
+    else:
+        pass
+        # Close the cursor and connection
+    finally:
+        cursor1.close()
+        conn.close()
